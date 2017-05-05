bea91550c3b770633ea439f80ff7c386fc981b34
Waldo <waldospek@gmail.com>
2017-05-05 18:26:17 -0300

    Switch to MySQL DB.

--- socialdata_site/settings.py
+++ socialdata_site/settings.py
@@ -89,10 +89,24 @@ os.environ['wsgi.url_scheme'] = 'https'
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'socialdata_site',
+        'USER': 'socialdata_user',
+        'PASSWORD': 'antisocial',
+        'HOST': 'localhost',
+        'PORT': '',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
