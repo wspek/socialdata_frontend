0f33aa9b62980f38b11bfcfb379a3b80520a8e22
Waldo <waldospek@gmail.com>
2017-05-10 12:59:57 -0300

    Modifications to daemonize celery

--- contactlist_app/views.py
+++ contactlist_app/views.py
@@ -5,7 +5,7 @@ import pdb
 from celery import task
 from celery.contrib import rdb
 from celery.result import AsyncResult
-from celery.app.task import Task
+# from celery.app.task import Task
 import crawler
 from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
